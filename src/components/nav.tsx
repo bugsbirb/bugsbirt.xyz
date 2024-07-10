15fb7d26ee5a85285325fd1faf0fa68ad57e8f48
Bugs <deezbird2768@gmail.com>
2024-07-10 18:26:21 +0100

    ok

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,79 +1,80 @@
-"use client"
+"use client";
 import React, { useState } from 'react';
 import '../app/globals.css'; 
+
 const Navbar = () => {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-  
-    const toggleMenu = () => {
-      setIsMenuOpen(!isMenuOpen);
-    };
-  
-    return (
-      <nav className="bg-gray-900 bg-opacity-20 backdrop-blur-md border border-gray-900 border-b-gray-800 border-b-opacity-20 in-line-flex items-center justify-between flex-wrap border-b">
-        <div className="w-full flex items-center justify-between mx-auto p-4">
-          <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-            <img
-              className="w-10 h-10 rounded-full"
-              src="https://cdn.discordapp.com/avatars/795743076520820776/0e0723ce46265094ea7dda085822c6a0.webp?size=1024&format=webp&width=0&height=256"
-              alt="Rounded avatar"
-            />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">
-              bugsbirt
-            </span>
-          </a>
-          <button
-            onClick={toggleMenu}
-            type="button"
-            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            aria-controls="navbar-default"
-            aria-expanded={isMenuOpen ? 'true' : 'false'}
-          >
-            <span className="sr-only">Open main menu</span>
-            <svg
-              className="w-5 h-5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 17 14"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M1 1h15M1 7h15M1 13h15"
-              />
-            </svg>
-          </button>
-          <div
-            className={`${
-              isMenuOpen ? 'block' : 'hidden'
-            } w-full md:block md:w-auto`}
-            id="navbar-default"
-          >
-            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-600 bg-gray-800 bg-opacity-20 rounded-lg  md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-gray-900 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-800">
-              <li>
-                <a
-                  href="/"
-                  className="block py-2 px-3 text-white rounded hover:bg-gray-700 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                  aria-current="page"
-                >
-                  Home
-                </a>
-              </li>
-              <li>
-                <a
-                  href="/projects"
-                  className="block py-2 px-3 text-white rounded hover:bg-gray-700 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
-                  Projects
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    );
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
   };
-  
-  export default Navbar;
+
+  return (
+    <nav className="bg-gray-900 bg-opacity-20 backdrop-blur-md border border-gray-900 border-b-gray-800 border-b-opacity-20 flex items-center justify-between p-4">
+      <div className="flex items-center justify-between w-full">
+        <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+          <img
+            className="w-10 h-10 rounded-full"
+            src="https://cdn.discordapp.com/avatars/795743076520820776/0e0723ce46265094ea7dda085822c6a0.webp?size=1024&format=webp&width=0&height=256"
+            alt="Rounded avatar"
+          />
+          <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">
+            bugsbirt
+          </span>
+        </a>
+        <button
+          onClick={toggleMenu}
+          type="button"
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          aria-controls="navbar-default"
+          aria-expanded={isMenuOpen ? 'true' : 'false'}
+        >
+          <span className="sr-only">Open main menu</span>
+          <svg
+            className="w-5 h-5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 17 14"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M1 1h15M1 7h15M1 13h15"
+            />
+          </svg>
+        </button>
+      </div>
+      <div
+        className={`${
+          isMenuOpen ? 'block' : 'hidden'
+        } absolute top-full left-0 w-full md:relative md:top-auto md:left-auto md:w-auto md:flex`}
+        id="navbar-default"
+      >
+        <ul className="font-medium flex flex-col p-4 mt-4 border border-gray-600 bg-gray-950 bg-opacity-20 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-gray-900 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-800">
+          <li>
+            <a
+              href="/"
+              className="block py-2 px-3 text-white rounded hover:bg-gray-700 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              aria-current="page"
+            >
+              Home
+            </a>
+          </li>
+          <li>
+            <a
+              href="/projects"
+              className="block py-2 px-3 text-white rounded hover:bg-gray-700 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+            >
+              Projects
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
