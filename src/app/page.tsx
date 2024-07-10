9fd007519cd8b15c0d1ee43a9fab9cc56caafe22
Bugs <deezbird2768@gmail.com>
2024-07-10 18:35:08 +0100

    weaeaw

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,39 @@
 import React from "react";
 import Navbar from "../components/nav";
 import '../app/globals.css'; 
+
 export default function Home() {
   return (
     <main className="flex flex-col min-h-screen bg-gray-900 dark:bg-gray-900">
       <Navbar />
       <div className="flex-grow flex flex-col items-center justify-center px-4">
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center text-center">
           <img
             src="/bugsbirt.png"
             alt="bugs"
             className="w-64 h-64 mb-10 rounded-full"
           />
-          <section className="text-center">
-            <h1 className="text-8xl font-bold text-white">
+          <section>
+            <h1 className="text-5xl sm:text-8xl font-bold text-white">
               bugsbirt
             </h1>
-            <p className="mt-6 text-3xl text-gray-300">
+            <p className="mt-6 text-2xl sm:text-3xl text-gray-300">
               I own an army of birds
             </p>
-            <div className="mt-10 flex space-x-6 justify-center font-bold">
+            <div className="mt-10 flex flex-col sm:flex-row sm:space-x-6 justify-center font-bold">
               <a
                 href="/projects"
+                className="cursor-pointer"
               >
-                <button className="px-10 py-5 bg-indigo-600 text-white rounded-lg shadow-lg hover:bg-indigo-700 transition-colors">
+                <button className="px-8 py-4 sm:px-10 sm:py-5 bg-indigo-600 text-white rounded-lg shadow-lg hover:bg-indigo-700 transition-colors">
                   Projects
                 </button>
               </a>
               <a
                 href="https://ko-fi.com/bugsbirt"
-
+                className="cursor-pointer mt-4 sm:mt-0"
               >
-                <button className="px-10 py-5 bg-transparent border border-white text-white rounded-lg shadow-lg hover:bg-gray-800 transition-colors">
+                <button className="px-8 py-4 sm:px-10 sm:py-5 bg-transparent border border-white text-white rounded-lg shadow-lg hover:bg-gray-800 transition-colors">
                   Donate
                 </button>
               </a>
