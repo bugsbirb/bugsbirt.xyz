7996ad8a04b1f823233a8a343a650e4638e20c87
Bugs <deezbird2768@gmail.com>
2024-07-16 01:03:37 +0100

    additions i like

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,11 @@ export default function RootLayout({ children }: RootLayoutProps) {
       <head>
       <meta charSet="UTF-8" />
         <link rel="icon" type="image/png" href="/bugsbirt.png"/>
+        <meta name="description" content="i like to make things sometimes"/>
+        <meta name="theme-color" content="#5b65e8" />
+        <meta name="url" content="http://bugsbirt.xyz"/>
+        <link rel="icon" href="/bugsbirt.png" type="image/x-icon" />
+
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>bugsbirt</title>
       </head>
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -26,7 +26,7 @@ export default function Projects() {
                         <div className="flex flex-col items-center pb-10 pt-6 relative">
                             <img className="w-32 h-32 mb-4 rounded-full shadow-lg" src="/astrobirb.gif" alt="Astro Birb"/>
                             <h5 className="mb-2 text-xl font-medium text-white">Astro Birb</h5>
-                            <span className="text-lg text-gray-500">A nice staff management</span>
+                            <span className="text-lg text-gray-200">A nice staff management bot</span>
                             <div className="flex mt-4 justify-center sm:justify-start"> 
                                 <a href="https://discord.gg/birb" className="inline-flex items-center px-4 py-2 text-base font-medium text-center text-white bg-indigo-500 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">Join</a>
                                 <a href="https://astrobirb.dev" className="py-2 px-4 ml-2 text-base font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100">Website</a>
@@ -44,8 +44,8 @@ export default function Projects() {
                         <div className="flex flex-col items-center pb-10 pt-6">
                             <img className="w-32 h-32 mb-4 rounded-full shadow-lg" src="/birbmoney.png" alt="Bugsy's Commissions"/>
                             <h5 className="mb-2 text-xl font-medium text-white">Bugsy's Commissions</h5>
-                            <span className="text-lg text-gray-500">Sometimes I like to sell bot stuff</span>
-                            <div className="flex mt-4 justify-center sm:justify-start"> {/* Center buttons on mobile, start on desktop */}
+                            <span className="text-lg text-gray-200">Sometimes I like to sell bot stuff</span>
+                            <div className="flex mt-4 justify-center sm:justify-start"> 
                                 <a href="https://discord.gg/V8UrYsX9u6" className="inline-flex items-center px-4 py-2 text-base font-medium text-center text-white bg-indigo-500 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">Join</a>
                             </div>
                         </div>
