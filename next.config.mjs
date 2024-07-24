f0fa918ca0eb1543e55dec16aa4f531dd1f57932
Bugs <deezbird2768@gmail.com>
2024-07-24 13:35:33 +0100

    ea

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,32 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+
+    async redirects() {
+        return [
+          {
+            source: '/astrobirb', 
+            destination: 'https://discord.gg/DhWdgfh3hN',
+            permanent: true, 
+          },
+          {
+            source: '/birb', 
+            destination: 'https://discord.gg/DhWdgfh3hN',
+            permanent: true,             
+          },
+          {
+            source: 'ko-fi', 
+            destination: 'https://ko-fi.com/bugsbirt',
+            permanent: true,      
+          },
+          {
+            source: 'hitman', 
+            destination: 'https://bugsbirt.xyz',
+            permanent: true,              
+          },
+
+        ];
+      },
+
+};
 
 export default nextConfig;
