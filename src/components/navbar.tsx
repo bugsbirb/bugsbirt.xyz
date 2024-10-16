2a149c12953a4961dac3f830be14c689a764edc0
Bugs <deezbird2768@gmail.com>
2024-10-16 21:43:17 +0100

    oik

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -12,7 +12,7 @@ export default function Nav() {
     <nav className="glassy bg-white fixed w-full z-20 top-0 left-0 border-b border-gray-200 ">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a onClick={() => push("/")} className="hand flex items-center space-x-3 rtl:space-x-reverse">
-          <img src="/bugsbirt.png" className="h-8 rounded-lg" alt="Flowbite Logo" />
+          <img src="/bugsbirt.png" className="h-8 rounded-lg" alt="Bugsy" />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Bugsy</span>
         </a>
         <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
@@ -84,17 +84,9 @@ export default function Nav() {
               >
                 Socials
               </a>
-            </li>      
-            <li>
-              <a
-                onClick={() => push("/tools")}
-                className="yourmum hand block py-2 px-3 text-gray-300 rounded  md:hover:bg-transparent md:hover:text-indigo-400 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                Tools
-              </a>
             </li>                           
           </ul>
-   
+  
         </div>
       </div>
     </nav>
